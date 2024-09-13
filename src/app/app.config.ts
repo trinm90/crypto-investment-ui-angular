85fca6acbfaa10305531220570576018f5ec7215
Nguyen Minh Tri <tri.nguyen@naivecat.com>
2024-09-13 21:20:59 +0700

    Add angular/fire package

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,20 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
+import { environment } from '../environments/environment';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp } from 'firebase/app';
+import { AnalyticsModule, UserTrackingService } from '@angular/fire/analytics';
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideHttpClient(withInterceptorsFromDi())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    AnalyticsModule,
+    UserTrackingService
+  ]
 };
